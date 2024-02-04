b6efb24b6a7f2f20aaacdce50872ecdae004dd00
jmartinm2021 <j.martinm.2021@alumnos@urjc.es>
2024-02-04 20:30:00 +0100

    Primer error arreglado

--- src/go_forward_functions/functions.cpp
+++ src/go_forward_functions/functions.cpp
@@ -1,3 +1,19 @@
+// Copyright 2024 Intelligent Robotics Lab
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+
+
 #include <iostream>
 
 #include "rclcpp/rclcpp.hpp"
@@ -15,9 +31,9 @@ namespace go_forward
 ForwardNode::ForwardNode()
 : Node("forward_node")
 {
-    publisher_ = create_publisher<geometry_msgs::msg::Twist>("int_topic", 10);
+    publisher_ = create_publisher<geometry_msgs::msg::Twist>("vel", 10);
     subscriber_ = create_subscription<kobuki_ros_interfaces::msg::BumperEvent>(
-        "int_topic", 10, 
+        "bumper", 10, 
         std::bind(&ForwardNode::bumper_callback, this, _1))
     timer_ = create_wall_timer(
         5s, std::bind(&ForwardNode::timer_callback, this));
